8d2725cccb665e178b3705a00d1a3d103388532e
Sungcheol Kim <kimsung@us.ibm.com>
2024-08-29 13:44:47 -0400

    add iterator

--- p-composite/composite_base.py
+++ p-composite/composite_base.py
@@ -53,28 +53,6 @@ class Composite(Component):
         return " ".join(results)
 
 
-class Composite(Component):
-    def __init__(self) -> None:
-        self._children: List[Component] = []
-
-    def add(self, component: Component) -> None:
-        self._children.append(component)
-        component.parent = self
-
-    def remove(self, component: Component) -> None:
-        self._children.remove(component)
-        component.parent = None
-
-    def is_composite(self) -> bool:
-        return True
-
-    def operation(self) -> str:
-        results = []
-        for child in self._children:
-            results.append(child.operation())
-        return f"Branch({'+'.join(results)})"
-
-
 def client_code(component: Component) -> None:
     print(f"RESULT: {component.operation()}", end="")
 
